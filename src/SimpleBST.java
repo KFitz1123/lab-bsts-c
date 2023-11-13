6f84d726f9739fa96b2fc4b122f8c4ef417c6670
Rene_Urias_Jr <uriasren@grinnell.edu>
2023-11-13 11:02:38 -0600

    Set Iterative

--- src/SimpleBST.java
+++ src/SimpleBST.java
@@ -7,7 +7,7 @@ import java.util.function.BiConsumer;
 /**
  * A simple implementation of binary search trees.
  */
-public class SimpleBST<K,V> implements SimpleMap<K,V> {
+public class SimpleBST<K, V> implements SimpleMap<K, V> {
 
   // +--------+------------------------------------------------------
   // | Fields |
@@ -16,7 +16,7 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
   /**
    * The root of our tree. Initialized to null for an empty tree.
    */
-  BSTNode<K,V> root;
+  BSTNode<K, V> root;
 
   /**
    * The comparator used to determine the ordering in the tree.
@@ -27,7 +27,7 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
    * The size of the tree.
    */
   int size;
-  
+
   /**
    * A cached value (useful in some circumstances.
    */
@@ -38,8 +38,7 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
   // +--------------+
 
   /**
-   * Create a new binary search tree that orders values using the 
-   * specified comparator.
+   * Create a new binary search tree that orders values using the specified comparator.
    */
   public SimpleBST(Comparator<? super K> comparator) {
     this.comparator = comparator;
@@ -49,8 +48,7 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
   } // SimpleBST(Comparator<K>)
 
   /**
-   * Create a new binary search tree that orders values using a 
-   * not-very-clever default comparator.
+   * Create a new binary search tree that orders values using a not-very-clever default comparator.
    */
   public SimpleBST() {
     this((k1, k2) -> k1.toString().compareTo(k2.toString()));
@@ -63,27 +61,48 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
 
   @Override
   public V set(K key, V value) {
-    BSTNode<K,V> current = this.root;
-    V temp;
-    if(key == null){
+    if (key == null) {
       throw new NullPointerException("null key");
-    }else {
-      for(int i = 0; i < this.size(); i++){
-      int comp = comparator.compare(key, current.key);
-      if(comp == 0){
-       temp = current.value;
-       current.value = value;
-        return temp;
-      } else if(comp > 1){
-        current = current.right;
-      }else{
-        current = current.left;
-      }
-      }
-      
     }
 
-  } // set(K,V)
+    BSTNode<K, V> newNode = new BSTNode<>(key, value);
+
+    if (root == null) {
+      root = newNode;
+      size++;
+      return null;
+    }
+
+    BSTNode<K, V> current = root;
+    BSTNode<K, V> parent = null;
+
+    while (current != null) {
+      int comp = comparator.compare(key, current.key);
+
+      if (comp == 0) {
+        V oldValue = current.value;
+        current.value = value;
+        return oldValue;
+      } else if (comp < 0) {
+        parent = current;
+        current = current.left;
+      } else {
+        parent = current;
+        current = current.right;
+      }
+    }
+
+    // If we get here, the key is not present
+    int comp = comparator.compare(key, parent.key);
+    if (comp < 0) {
+      parent.left = newNode;
+    } else {
+      parent.right = newNode;
+    }
+
+    size++;
+    return null;
+  }
 
   @Override
   public V get(K key) {
@@ -95,23 +114,23 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
 
   @Override
   public int size() {
-    return 0;           // STUB
+    return 0; // STUB
   } // size()
 
   @Override
   public boolean containsKey(K key) {
-    return false;       // STUB
+    return false; // STUB
   } // containsKey(K)
 
   @Override
   public V remove(K key) {
-    return null;        // STUB
+    return null; // STUB
   } // remove(K)
 
   @Override
   public Iterator<K> keys() {
     return new Iterator<K>() {
-      Iterator<BSTNode<K,V>> nit = SimpleBST.this.nodes();
+      Iterator<BSTNode<K, V>> nit = SimpleBST.this.nodes();
 
       @Override
       public boolean hasNext() {
@@ -133,7 +152,7 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
   @Override
   public Iterator<V> values() {
     return new Iterator<V>() {
-      Iterator<BSTNode<K,V>> nit = SimpleBST.this.nodes();
+      Iterator<BSTNode<K, V>> nit = SimpleBST.this.nodes();
 
       @Override
       public boolean hasNext() {
@@ -176,7 +195,7 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
   /**
    * Dump a portion of the tree to some output location.
    */
-  void dump(PrintWriter pen, BSTNode<K,V> node, String indent) {
+  void dump(PrintWriter pen, BSTNode<K, V> node, String indent) {
     if (node == null) {
       pen.println(indent + "<>");
     } else {
@@ -187,12 +206,12 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
       } // if has children
     } // else
   } // dump
-  
+
   /**
-   * Get the value associated with a key in a subtree rooted at node.  See the
-   * top-level get for more details.
+   * Get the value associated with a key in a subtree rooted at node. See the top-level get for more
+   * details.
    */
-  V get(K key, BSTNode<K,V> node) {
+  V get(K key, BSTNode<K, V> node) {
     if (node == null) {
       throw new IndexOutOfBoundsException("Invalid key: " + key);
     }
@@ -207,13 +226,12 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
   } // get(K, BSTNode<K,V>)
 
   /**
-   * Get an iterator for all of the nodes. (Useful for implementing the 
-   * other iterators.)
+   * Get an iterator for all of the nodes. (Useful for implementing the other iterators.)
    */
-  Iterator<BSTNode<K,V>> nodes() {
-    return new Iterator<BSTNode<K,V>>() {
+  Iterator<BSTNode<K, V>> nodes() {
+    return new Iterator<BSTNode<K, V>>() {
 
-      Stack<BSTNode<K,V>> stack = new Stack<BSTNode<K,V>>();
+      Stack<BSTNode<K, V>> stack = new Stack<BSTNode<K, V>>();
       boolean initialized = false;
 
       @Override
@@ -223,7 +241,7 @@ public class SimpleBST<K,V> implements SimpleMap<K,V> {
       } // hasNext()
 
       @Override
-      public BSTNode<K,V> next() {
+      public BSTNode<K, V> next() {
         checkInit();
         // TODO Auto-generated method stub
         return null;
